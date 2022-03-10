1651b4ddf9d9908f5ef07b4c478bd9f513cd9b72
Akitoki <a_head_zfx_aktk@icloud.com>
2022-03-10 23:33:20 +0900

    3月10日記事更新

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,6 +85,15 @@ $(function() {
       spMenuBtn.toggleClass('open')
     })
   }
+  $('.blog-wrap a[href^="#"]').on('click', function() {
+    const speed = 500
+    const href = $(this).attr("href")
+    const headerHeight = $('.header').outerHeight()
+    const target = $(href == "#" || href == "" ? 'html' : href)
+    const position = target.offset().top - headerHeight
+    $('html').animate({scrollTop:position}, speed, 'swing')
+    return false;
+  });
 })
 $(window).on('load', function(){
   $('body').removeClass('fadeout');
